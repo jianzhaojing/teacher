58947dfb5ac23db61fcd8a24773fa041d0533b04
jianzhaojing <2653723104@qq.com>
2018-07-24 16:34:11 +0800

    Signed-off-by: jianzhaojing <2653723104@qq.com> 教学系统

--- config/app.php
+++ config/app.php
@@ -19,7 +19,7 @@ return [
     // 应用地址
     'app_host'               => '',
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => true,
     // 应用Trace
     'app_trace'              => false,
     // 是否支持多模块
@@ -54,11 +54,11 @@ return [
     // +----------------------------------------------------------------------
 
     // 默认模块名
-    'default_module'         => 'index',
+    'default_module'         => 'admin',
     // 禁止访问模块
     'deny_module_list'       => ['common'],
     // 默认控制器名
-    'default_controller'     => 'Index',
+    'default_controller'     => 'Student',
     // 默认操作名
     'default_action'         => 'index',
     // 默认验证器
